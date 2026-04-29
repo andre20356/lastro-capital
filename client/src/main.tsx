69b3c4a0ee1154e10c9fb22dcf1608eca21e0c9a
vamoscomtudo913@gmail.com <vamoscomtudo913@gmail.com>
2026-04-29 11:43:04 -0300

    fix: rotas funcionando + navbar corrigido

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./App";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout><Home /></Layout>} />
+      <Route path="/cadastro" element={<Layout><Cadastro /></Layout>} />
+      <Route path="/login" element={<Layout><Login /></Layout>} />
+    </Routes>
+  </BrowserRouter>
 );
